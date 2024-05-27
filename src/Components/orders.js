dc607f2b1c6234e1bb1c8745bc9b15c75c5ee096
manelcaimari <manelcaimari1@gmail.com>
2024-05-27 19:10:30 +0200

    hola

---
+++ src/Components/orders.js
@@ -0,0 +1,88 @@
+class orders extends HTMLElement {
+    constructor () {
+      super()
+      this.shadow = this.attachShadow({ mode: 'open' })
+    }
+  
+    connectedCallback () {
+      
+      this.data = {
+      }
+  
+      this.render()
+    }
+  
+    render () {
+      this.shadow.innerHTML =
+         /*html*/`
+        <style>
+       .orders{
+    display:grid;
+
+}
+
+.order-title h2::first-letter{
+    text-transform: capitalize;
+    
+}
+
+.order-title h2{
+    font-size:1.4rem ;
+    font-weight: 900;
+    text-align:center;
+
+}
+
+.order-text ::first-letter{
+    text-transform: capitalize;
+
+}
+
+.order-text{
+    text-align:center;
+    font-size:18px;
+   
+}
+
+.go-home{
+    text-align: center;
+
+}
+
+.go-home button::first-letter{
+    text-transform: capitalize;
+
+}
+
+.go-home button{
+    background-color: white;
+    color:  hsl(240, 33%, 1%);
+    border: none;
+    border-radius: 13px;
+    padding: 7px 0;
+    text-align: center;
+    text-decoration: none;
+    font-size: 16px;
+    width: 70%; 
+    cursor: pointer;
+    font-weight: 700;
+
+}
+        </style>
+  
+  <section class="orders">
+  <div class="order-title">
+      <h2>pedido realizado con èxito.</h2>
+  </div>
+  <div class="order-text">
+      <p>en breve recibirá un correo con los detalles. La rederencia de su pedido es 00000000002</p>
+  </div>
+  <div class="go-home">
+      <a href="#"><button>volver a inicio</button></a>
+  </div>
+</section>
+        `
+      }
+  }
+  
+  customElements.define('orders-component', orders)
