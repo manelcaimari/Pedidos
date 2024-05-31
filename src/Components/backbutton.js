36484fd6f127cdca3ae38160d3c66996a8c7d35a
manelcaimari <manelcaimari1@gmail.com>
2024-05-31 14:19:26 +0200

    mejoras

--- src/Components/backbutton.js
+++ src/Components/backbutton.js
@@ -10,7 +10,7 @@ class backbutton extends HTMLElement {
 
   render () {
     this.shadow.innerHTML =
-        /*html*/`
+      /*html*/`
         <style>
           .svg {
               height: 1.8rem;
@@ -32,8 +32,6 @@ class backbutton extends HTMLElement {
             display: block;
             height: 100%; 
             width: 100%;
-       
-             
           }
           svg{
             height: 1.8rem;
@@ -46,7 +44,7 @@ class backbutton extends HTMLElement {
           <div class="svg">
               <button><a href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>arrow-left</title><path d="M20,11V13H8L13.5,18.5L12.08,19.92L4.16,12L12.08,4.08L13.5,5.5L8,11H20Z" /></svg></a></button>
           </div>
-        `
+      `
   }
 }
   
