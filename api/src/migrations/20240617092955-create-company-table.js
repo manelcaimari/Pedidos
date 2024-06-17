d70871cd840dba18fd8ac5fa76edcb03ac454e1d
manelcaimari <manelcaimari1@gmail.com>
2024-06-17 13:42:41 +0200

    creado la carpeta de migrations con las tablas user (usuarios) y company (compañia)

---
+++ api/src/migrations/20240617092955-create-company-table.js
@@ -0,0 +1,50 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('companies', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      commercialAddress: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      fiscalAddress: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      commercialName: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      fiscalName: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      vatNumber: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      deletedAt: {
+        type: Sequelize.DATE
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('companies')
+  }
+}
