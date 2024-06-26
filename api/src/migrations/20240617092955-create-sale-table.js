9478ce120727fe809542a78094bc080be4bd5171
manelcaimari <manelcaimari1@gmail.com>
2024-06-26 14:33:59 +0200

    creado bien la api de sale

---
+++ api/src/migrations/20240617092955-create-sale-table.js
@@ -0,0 +1,50 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('sales', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    },
+    customerId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    reference: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    totalBasePrice: {
+        type: Sequelize.DECIMAL(10, 2), 
+        allowNull: false
+    },
+    saleDate: {
+        type: Sequelize.DATEONLY,
+        allowNull: false
+    },
+    saleTime: {
+        type: Sequelize.TIME,
+        allowNull: true 
+    },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      deletedAt: {
+        type: Sequelize.DATE
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('sales')
+  }
+}
